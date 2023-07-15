e0251ac02aff05d883faeeeeac6cec889e878a56
1DoraGON <moulaykadii2001@gmail.com>
2023-07-15 21:04:33 +0200

    navbar, stories and footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 
-import {FlexContent, Hero, Sales} from './components/index'
-import { heroapi, highlight, popularsales, sneaker, toprateslaes } from './data/data.js'
+import {FlexContent, Footer, Hero, Navbar, Sales, Stories} from './components/index'
+import { footerAPI, heroapi, highlight, popularsales, sneaker, story, toprateslaes } from './data/data.js'
 function App() {
   return (
     <>
-    <main className='flex flex-col gap-16 relative'>
-      <Hero heroapi= {heroapi} />
-      <Sales sales = {popularsales} ifExists />
-      <FlexContent endpoint={highlight} isLeft />
-      <Sales sales = {toprateslaes} />
-      <FlexContent endpoint={sneaker} />
-    </main>
+      <Navbar />
+      <main className='flex flex-col gap-16 relative'>
+        <Hero heroapi= {heroapi} />
+        <Sales sales = {popularsales} ifExists />
+        <FlexContent endpoint={highlight} isLeft />
+        <Sales sales = {toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerapi={footerAPI}/>
     </>
   )
 }
