3ddcafb9196451896ae50747989096c996a9de55
1DoraGON <moulaykadii2001@gmail.com>
2023-07-16 15:33:16 +0200

    cart design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
-import {FlexContent, Footer, Hero, Navbar, Sales, Stories} from './components/index'
+import {Cart, FlexContent, Footer, Hero, Navbar, Sales, Stories} from './components/index'
 import { footerAPI, heroapi, highlight, popularsales, sneaker, story, toprateslaes } from './data/data.js'
 function App() {
   return (
     <>
       <Navbar />
+      <Cart />
       <main className='flex flex-col gap-16 relative'>
         <Hero heroapi= {heroapi} />
         <Sales sales = {popularsales} ifExists />
